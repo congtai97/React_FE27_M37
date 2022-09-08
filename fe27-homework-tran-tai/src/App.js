f3cdbfb3ea91bfc2b88b28b3a18fed6250867597
taitcpd01633 <trancongtai997@gmail.com>
2022-09-08 21:11:37 +0700

    HomeWork1

---
+++ fe27-homework-tran-tai/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { CardItem } from './components/CardItem';
+
+function App() {
+  return (
+    <div className="App">
+      <CardItem></CardItem>
+    </div>
+  );
+}
+
+export default App;
